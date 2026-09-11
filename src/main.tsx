f2521209628d6940c3462e6a1eb528987cd4458e
Zividynamics <codezivi@gmail.com>
2026-09-11 01:37:30 -0400

    chore: force safe pwa cache refresh for quick tools

--- src/main.tsx
+++ src/main.tsx
@@ -33,7 +33,7 @@ type PwaWindow = Window & {
 }
 
 const pwaWindow = window as PwaWindow
-const PWA_RESET_KEY = 'zivifactura.pwa-reset-v33'
+const PWA_RESET_KEY = 'zivifactura.pwa-reset-v34'
 
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault()
