d7897f600d286282ff51bfdca0d5d13f203a2656
Zividynamics <codezivi@gmail.com>
2026-09-11 00:06:57 -0400

    Remove quick tools patch imports

--- src/main.tsx
+++ src/main.tsx
@@ -20,8 +20,6 @@ import './polish-v24.css'
 import './polish-v27.css'
 import './polish-v28.css'
 import './auth-v29.css'
-import './quick-tools-fixes.css'
-import './quickToolsRuntimeFixes'
 
 type DeferredInstallPrompt = Event & {
   prompt?: () => Promise<void>
