f303b7ab50b1e0f0a2b415b31451df4b9c134d5f
Zividynamics <codezivi@gmail.com>
2026-09-17 21:34:20 -0400

    chore: bump PWA cache to v58

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'zivifactura-shell-v57'
+const CACHE = 'zivifactura-shell-v58'
 const APP_SHELL = [
-  '/manifest.webmanifest?v=57',
-  '/zivifactura-app-192-v43.png?v=57',
-  '/zivifactura-app-512-v56.svg?v=57',
+  '/manifest.webmanifest?v=58',
+  '/zivifactura-app-192-v43.png?v=58',
+  '/zivifactura-app-512-v56.svg?v=58',
 ]
 
 async function cacheCurrentBuild(cache) {
