d977bbba5fa7d2bb0983e9243d0ca45eab86ee4b
Zividynamics <codezivi@gmail.com>
2026-09-17 20:59:13 -0400

    fix: build legacy-compatible Android bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
-  build: { sourcemap: false },
+  plugins: [
+    react(),
+    legacy({
+      targets: ['Android >= 7', 'Chrome >= 61', 'Samsung >= 8'],
+      modernPolyfills: true,
+      renderLegacyChunks: true,
+    }),
+  ],
+  build: {
+    sourcemap: false,
+    target: 'es2018',
+  },
 })
